9734716809481b1d692ae92634a0a0a62ebd2a6e
TangJinXuan <38104211+TangJinXuan@users.noreply.github.com>
2021-09-21 20:46:54 -0400

    最大堆

---
+++ 502.cpp
@@ -0,0 +1,42 @@
+// 502. IPO
+// 假设 力扣（LeetCode）即将开始 IPO 。为了以更高的价格将股票卖给风险投资公司，力扣 希望在 IPO 之前开展一些项目以增加其资本。 由于资源有限，它只能在 IPO 之前完成最多 k 个不同的项目。帮助 力扣 设计完成最多 k 个不同项目后得到最大总资本的方式。
+
+// 给你 n 个项目。对于每个项目 i ，它都有一个纯利润 profits[i] ，和启动该项目需要的最小资本 capital[i] 。
+
+// 最初，你的资本为 w 。当你完成一个项目时，你将获得纯利润，且利润将被添加到你的总资本中。
+
+// 总而言之，从给定项目中选择 最多 k 个不同项目的列表，以 最大化最终资本 ，并输出最终可获得的最多资本。
+
+// 答案保证在 32 位有符号整数范围内。
+
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        vector<pair<int,int>> projects;
+        for(int i= 0;i<profits.size();++i)
+            projects.push_back(make_pair(capital[i],profits[i]));
+        sort(projects.begin(),projects.end());
+        int tempLine(0);
+        priority_queue<int> temp;
+        for(int i=0;i<k;++i)
+        {
+            for(;tempLine<projects.size();++tempLine)
+            {
+                // cout<<j->first<<" ";
+                if (projects[tempLine].first<=w)
+                    temp.push(projects[tempLine].second);
+                else
+                    break;
+            }
+            if(!temp.empty())
+            {
+                w+=temp.top();
+                temp.pop();
+            }
+            // cout<<maxValue<<endl;
+            else
+                break;
+        }
+        return w;
+    }
+};
