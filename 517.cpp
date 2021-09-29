b72946e9e505b24f1a6f43152751bacd5f54fa02
TangJinXuan <38104211+TangJinXuan@users.noreply.github.com>
2021-09-29 01:06:45 -0400

    暂未做完，答案疑似有问题

---
+++ 517.cpp
@@ -0,0 +1,35 @@
+517. 超级洗衣机
+假设有 n 台超级洗衣机放在同一排上。开始的时候，每台洗衣机内可能有一定量的衣服，也可能是空的。
+
+在每一步操作中，你可以选择任意 m (1 <= m <= n) 台洗衣机，与此同时将每台洗衣机的一件衣服送到相邻的一台洗衣机。
+
+给定一个整数数组 machines 代表从左至右每台洗衣机中的衣物数量，请给出能让所有洗衣机中剩下的衣物的数量相等的 最少的操作步数 。如果不能使每台洗衣机中衣物的数量相等，则返回 -1 。
+这题leetcode描述和答案好像有点问题，答案并不是只能将洗衣机的衣服送到“相邻”的洗衣机，还没做完，有空写完。
+class Solution {
+public:
+    int findMinMoves(vector<int>& machines) {
+        int n=machines.size();
+        int sumN(0);
+        for(auto i:machines)
+            sumN+=i;
+        if(sum%n!=0)
+            return -1;
+        vector<int>bal;
+        for(auto i:machines)
+            bal.push_back(i-aver);
+        int pos=0;
+        int neg=0;
+        int res=0;
+        while(pos<n&&neg<n)
+        {
+            while(bal[pos]<=0)
+                pos++;
+            while(bal[neg]>=0)
+                neg++;    
+            while(bal[pos]>0&&bal[neg]<0)
+            {
+                
+            }
+        }
+    }
+};
