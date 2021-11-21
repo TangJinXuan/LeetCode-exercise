e1fd592d26e17c18f473ac9b5188b0bf31a71708
TangJinXuan <38104211+TangJinXuan@users.noreply.github.com>
2021-11-21 17:20:22 -0500

    做完啦

---
+++ 1822.cpp
@@ -0,0 +1,30 @@
+1822. 数组元素积的符号
+已知函数 signFunc(x) 将会根据 x 的正负返回特定值：
+
+如果 x 是正数，返回 1 。
+如果 x 是负数，返回 -1 。
+如果 x 是等于 0 ，返回 0 。
+给你一个整数数组 nums 。令 product 为数组 nums 中所有元素值的乘积。
+
+返回 signFunc(product) 。
+
+ 
+class Solution {
+public:
+    int signFunc(int i)
+    {
+        if(i>0)
+            return 1;
+        if(i<0)
+            return -1;
+        return 0;
+    }
+    int arraySign(vector<int>& nums) {
+        int res(1);
+        for(auto i:nums)
+        {
+            res=res*signFunc(i);
+        }
+        return res;
+    }
+};
