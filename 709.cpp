2c005d10b30ca5c34029644da5c53b256d2a09c9
TangJinXuan <38104211+TangJinXuan@users.noreply.github.com>
2021-12-11 20:47:38 -0500

    复习复习

---
+++ 709.cpp
@@ -0,0 +1,15 @@
+709. 转换成小写字母
+给你一个字符串 s ，将该字符串中的大写字母转换成相同的小写字母，返回新的字符串。
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(auto i=0;i<s.size();++i)
+        {
+            if(s[i]>='A'&&s[i]<='Z')
+            {
+                s[i]=s[i]-'A'+'a';
+            }
+        }
+        return s;
+    }
+};
